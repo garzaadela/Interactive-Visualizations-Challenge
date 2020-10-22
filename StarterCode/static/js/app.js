7b3e366924e6f709ec33c278c84833966911291c
Adela Garza <a.garza045@gmail.com>
2020-10-21 22:59:07 -0500

    updated hw

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,16 @@
 d3.json("samples.json").then((data)=>{
     console.log(data);
+
+    data.names.forEach(menuData => {
+        d3.select("#selDataset").append("option").text(menuData).property("value", menuData)
+
+    });
 });
+
+
+dropDownData = 947
+
+function filterPlotData(dropDownData) {
+    let filteredData = data.samples.filter(sampleData => sampleData.id === dropDownData);
+    console.log(filteredData);
+}
