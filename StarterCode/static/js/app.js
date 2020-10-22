ca086531cf3ad5c6966d1b478482c5e7843e343f
Adela Garza <a.garza045@gmail.com>
2020-10-22 17:58:20 -0500

    updated hw

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,7 @@
+var dataGlobal = []
 d3.json("samples.json").then((data)=>{
     console.log(data);
+    dataGlobal = data;
 
     data.names.forEach(menuData => {
         d3.select("#selDataset").append("option").text(menuData).property("value", menuData)
@@ -8,12 +10,24 @@ d3.json("samples.json").then((data)=>{
 });
 
 function optionChanged(input) {
-    console.log(input)
+    filterPlotData(input)
+    filterPanelData(input)
 }
 
 dropDownData = 947
 
 function filterPlotData(dropDownData) {
-    let filteredData = data.samples.filter(sampleData => sampleData.id === dropDownData);
+    let filteredData = dataGlobal.samples.filter(sampleData => sampleData.id === dropDownData);
     console.log(filteredData);
+}
+
+
+function filterPanelData(subjectIdSelected) {
+    let filteredData = dataGlobal.metadata.filter(sampleData => sampleData.id == subjectIdSelected);
+    console.log(dataGlobal.metadata)
+    console.log(filteredData);
+    d3.select("#sample-metadata").html("")
+    Object.entries(filteredData[0]).forEach(([key, value]) => {
+        d3.select("#sample-metadata").append("div").text(`${key.toUpperCase()}: ${value}`);
+      });
 }
