bd2e16f79e17b64fb185d050d52c1f730286b7d2
Adela Garza <a.garza045@gmail.com>
2020-10-23 22:11:14 -0500

    updated hw

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,4 @@
-var dataGlobal = []
+let dataGlobal = []
 d3.json("samples.json").then((data)=>{
     console.log(data);
     dataGlobal = data;
@@ -14,11 +14,48 @@ function optionChanged(input) {
     filterPanelData(input)
 }
 
-dropDownData = 947
+
 
 function filterPlotData(dropDownData) {
     let filteredData = dataGlobal.samples.filter(sampleData => sampleData.id === dropDownData);
     console.log(filteredData);
+
+    let yticks = filteredData[0].otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    let barPlotData = [
+      {
+        y: yticks,
+        x: filteredData[0].sample_values.slice(0, 10).reverse(),
+        text: filteredData[0].otu_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
+      }
+    ];
+    let barPlotLayout = {
+      title: "Top 10 OTUs",
+      margin: { t: 30, l: 150 }
+    };
+    Plotly.newPlot("bar", barPlotData, barPlotLayout);
+
+    let trace1 = {
+        x: filteredData[0].otu_ids,
+        y: filteredData[0].sample_values,
+        mode: 'markers',
+        marker: {
+            color: filteredData[0].otu_ids,
+            size: filteredData[0].sample_values
+        }
+    };
+
+    let bubbleData = [trace1];
+
+    let layout = {
+        title: 'Sample Values vs OTU IDs',
+        showlegend: false,
+        height: 600,
+        width: 600
+    };
+
+    Plotly.newPlot('bubble', bubbleData, layout);
 }
 
 
